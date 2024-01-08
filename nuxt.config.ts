25115ce10727b78fcddaf923d373b83397c7d22a
ibnuhazar101 <ibnuhazar101@gmail.com>
2024-01-08 23:49:57 +0700

    setup deployment script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  target: "static",
+  router: {
+    base: '/seakuntech/'
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
