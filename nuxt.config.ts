6fb610770804a5d721b0c5399b1376316b6f1f4e
ibnuhazar101 <ibnuhazar101@gmail.com>
2024-01-09 12:07:05 +0700

    fix setup deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
+  // ssr: false,
+  // target: 'static',
   app: {
-    baseURL: '/seakuntech/'
+    baseURL: '/seakuntech/',
+    buildAssetsDir: 'assets'
   },
+  // router: {
+  //   base: "/seakuntech/"
+  // },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
