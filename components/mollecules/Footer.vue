da203b1ac8a5b6d5382f5785991c144a2e31873e
zuhriyal <ridhozuhriyal7@gmail.com>
2024-02-05 21:48:27 +0700

    update footer

--- components/mollecules/Footer.vue
+++ components/mollecules/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="footer lg:pt-6 xl:pt-12 mt-12">
+  <div class="footer lg:pt-6 xl:pt-12 mt-12 bg-[#2d2d2d]">
     <div
       class="tn:px-3 md:!container !text-[#EFEFEF] !text-sm p-4 md:grid md:grid-cols-2 md:justify-between lg:grid-cols-5 lg:gap-8 lg:justify-center"
     >
