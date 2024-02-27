978d69626410d7c20f771f3b8fc5917e5c87b03e
zuhriyal <ridhozuhriyal7@gmail.com>
2024-02-27 16:42:48 +0700

    3a30c9d

---
+++ _nuxt/3425b51.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[61],{491:function(t,n,e){"use strict";e.r(n);var o={props:{dataOrder:{type:Object,default:function(){}}},methods:{}},c=e(51),component=Object(c.a)(o,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/dc40c42.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[59],{489:function(e,t,n){"use strict";n.r(t);n(204);var c={props:{pkg:{type:Object,default:function(){}},selectedPackage:{type:Object,default:function(){}},id:{type:Number,default:0}},computed:{}},l=n(51),component=Object(l.a)(c,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
